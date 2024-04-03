e651b393bf2e4d87905577ec3eff12090737c041
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-03 17:47:21 +0400

    changed postgres settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
