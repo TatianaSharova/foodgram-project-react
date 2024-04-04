fdc860c911c02423b49e6278fba18e69a287a738
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-04 08:04:13 +0400

    changed db sett

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
-#SECRET_KEY = 'django-insecure-dy0@f7+c(6l&(ht09yyneadv7dexkgpx#@ba8$y72js4$kzav)'
+# SECRET_KEY = 'django-insecure-dy0@f7+c(6l&(ht09yyneadv7dexkgpx#@ba8$y72js4$kzav)'
 
 DEBUG = os.getenv('DEBUG_STATUS', 'False').lower() > os.getenv('DEBUG_STATUS', 'False')
-#DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 # ALLOWED_HOSTS = ['foodgram-study.ddns.net', '127.0.0.1', 'localhost']
