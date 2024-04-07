e0a087f68558859181eea6a1240de3f971a0847d
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-07 14:57:34 +0400

    return sett

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -175,10 +175,10 @@ DJOSER = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
