52736eb742eca73488a0c91f68445b639d995112
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-10 17:47:33 +0400

    db

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram_password'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
