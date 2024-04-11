036b19f2ed0f3378926a66ae9d5059499c7f71da
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-11 12:03:09 +0400

    flake8

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -18,7 +18,7 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('author','tags', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
