2ca214e4babe94e4a92d8df9b1303e30417d21d9
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-04-18 15:59:44 +0400

    add __str__ to User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -17,3 +17,6 @@ class User(AbstractUser):
         ordering = ('-date_joined',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
